b8a69b05f3c31ffca986a0d15e042382ca57f32b
MustapheMS <moztaphem9@gmail.com>
2023-05-22 09:15:22 +0300

    Largest Two Numbers Done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,11 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+let X = 10;
+let Y = 8;
+
+if (X > Y) 
+{
+    console.log("The largest number out of the two numbers is X.");
+} else if (Y > X) {
+    console.log("The largest number out of the two numbers is y.");
+} else {
+    console.log("Both numbers are equal.");
+  }
